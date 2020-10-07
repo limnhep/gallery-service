bf238034005d6174e9fc5f3abb1aff91ecea176f
Harris Li <halisd1@gmail.com>
2020-10-06 21:43:03 -0700

    Updated Animations

--- client/src/components/GalleryModal.jsx
+++ client/src/components/GalleryModal.jsx
@@ -2,7 +2,7 @@
 /* eslint-disable react/prop-types */
 import React, { useState } from 'react';
 import { TransitionGroup, CSSTransition } from 'react-transition-group';
-import styled from 'styled-components';
+// import styled, { css } from 'styled-components';
 
 import GallerySaveShareModal from './subcomponents/GallerySaveShareModal.jsx';
 import {
@@ -21,21 +21,16 @@ import {
   IconContainer,
 } from '../../styled/galleryModal';
 
-const transitionStyles = styled.div`
-&:enter { opacity: 0; },
-&:enter-active { opacity: 1;
-  transition: opacity 1000ms ease-in;},
-&:exit { opacity: 1 ;},
-&:exit-active: { opacity: 0 ;
-  transition: opacity 1000ms ease-in;}},
-`;
-
 const GalleryModal = ({
   images, changeImage, closeModal, favorites, handleAddCategory, handleToggleFavorite, savedListing, selectedImage,
 }) => {
   const totalPhotos = images.length;
   const [modalState, setModalState] = useState(0);
-  const [bool, setBool] = useState(true);
+
+  const fadeInOut = {
+    
+  }
+
   let imageIndex;
   images.forEach((image, index) => {
     if (image.url === selectedImage) {
@@ -61,17 +56,17 @@ const GalleryModal = ({
         </IconContainer>
       </NavBar>
       <ImageFrame>
-        {imageIndex > 0 ? <ImageFrameLeftButton onClick={() => {changeImage(images[imageIndex - 1].url), setBool(!bool)}}><ImageArrowButton src="https://airbnb-bougie.s3-us-west-1.amazonaws.com/icons/left-arrow2.png" alt="img" /></ImageFrameLeftButton> : ''}
-        <CSSTransition
-          in={bool}
-          timeout={1000}
-          className={transitionStyles}
-          classNames={transitionStyles}
-          onEntered={() => setBool(!bool)}
-          unmountOnExit
-        >
-          <ImageFramePhoto src={selectedImage} />
-        </CSSTransition>
+        {imageIndex > 0 ? <ImageFrameLeftButton onClick={() => changeImage(images[imageIndex - 1].url)}><ImageArrowButton src="https://airbnb-bougie.s3-us-west-1.amazonaws.com/icons/left-arrow2.png" alt="img" /></ImageFrameLeftButton> : ''}
+        <TransitionGroup>
+          <CSSTransition
+            key={selectedImage}
+            timeout={350}
+            classNames="fade"
+            appear
+          >
+            <ImageFramePhoto src={selectedImage} />
+          </CSSTransition>
+        </TransitionGroup>
         {images[imageIndex].comment ? <ImageFrameComment>{images[imageIndex].comment}</ImageFrameComment> : null}
         {imageIndex < totalPhotos - 1 ? <ImageFrameRightButton onClick={() => changeImage(images[imageIndex + 1].url)}><ImageArrowButton src="https://airbnb-bougie.s3-us-west-1.amazonaws.com/icons/right-arrow2.png" alt="img" /></ImageFrameRightButton> : ''}
       </ImageFrame>
